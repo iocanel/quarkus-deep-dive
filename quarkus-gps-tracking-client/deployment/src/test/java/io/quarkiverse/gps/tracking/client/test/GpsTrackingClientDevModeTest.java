80c2ac44277dfbbc118145c0c549152484089997
Ioannis Canellos <iocanel@gmail.com>
2022-09-26 17:25:50 +0300

    test: test the devservice

--- quarkus-gps-tracking-client/deployment/src/test/java/io/quarkiverse/gps/tracking/client/test/GpsTrackingClientDevModeTest.java
+++ quarkus-gps-tracking-client/deployment/src/test/java/io/quarkiverse/gps/tracking/client/test/GpsTrackingClientDevModeTest.java
@@ -1,5 +1,17 @@
 package io.quarkiverse.gps.tracking.client.test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
+import javax.inject.Inject;
+
+import com.google.protobuf.Empty;
+
+import org.acme.tracker.client.TrackerClient;
+import org.amce.tracker.Tracker.Position;
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.jupiter.api.Assertions;
@@ -9,6 +21,9 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 import io.quarkus.test.QuarkusDevModeTest;
 
 public class GpsTrackingClientDevModeTest {
+
+    @Inject
+    public TrackerClient client;
 
     // Start hot reload (DevMode) test with your extension loaded
     @RegisterExtension
@@ -20,4 +35,14 @@ public class GpsTrackingClientDevModeTest {
         // Write your dev mode tests here - see the testing extension guide https://quarkus.io/guides/writing-extensions#testing-hot-reload for more information
         Assertions.assertTrue(true, "Add dev mode assertions to " + getClass().getName());
     }
+
+        @Test
+    public void writeYourOwnUnitTest() throws Exception {
+        Optional<String> url = ConfigProvider.getConfig().getOptionalValue("quarkus.tracker.url", String.class);
+        assertTrue(url.isPresent());
+        Position pos = client.gps().track(Empty.getDefaultInstance()).collect().first().await().indefinitely();
+        assertNotNull(pos.getLatitude());
+        assertNotNull(pos.getLongitude());
+    }
+
 }
