a9e6b76501ce91e11b670cc5bd7c92ece3e5f568
Ioannis Canellos <iocanel@gmail.com>
2022-09-27 10:50:24 +0300

    test: add test service

--- quarkus-gps-tracking-client/integration-tests/src/test/java/io/quarkiverse/gps/tracking/client/it/GpsTrackingClientResourceTest.java
+++ quarkus-gps-tracking-client/integration-tests/src/test/java/io/quarkiverse/gps/tracking/client/it/GpsTrackingClientResourceTest.java
@@ -1,21 +1,32 @@
 package io.quarkiverse.gps.tracking.client.it;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import javax.inject.Inject;
+
+import org.acme.tracker.client.TrackerClient;
+import org.amce.tracker.Tracker.Position;
 import org.junit.jupiter.api.Test;
 
+import com.google.protobuf.Empty;
+
 import io.quarkus.test.junit.QuarkusTest;
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
 
 @QuarkusTest
 public class GpsTrackingClientResourceTest {
 
+    @Inject
+    TrackerClient trackerClient;
+
     @Test
-    public void testHelloEndpoint() {
-        given()
-                .when().get("/gps-tracking-client")
-                .then()
-                .statusCode(200)
-                .body(is("Hello gps-tracking-client"));
+    public void testService() {
+        Multi<Position> positions = trackerClient.gps().track(Empty.getDefaultInstance());
+        assertNotNull(positions);
+        Uni<Position> first = positions.collect().first();
+        Position p = first.await().indefinitely();
+        assertNotNull(p.getLatitude());
+        assertNotNull(p.getLongitude());
     }
 }
